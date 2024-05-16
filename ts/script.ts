f2a173c823dba610d5c23881d3b4aa20961d84fc
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-05-16 15:43:59 -0600

    Calling buttons by their id

--- ts/script.ts
+++ ts/script.ts
@@ -1,1 +1,12 @@
-console.log(3);
+// LLAMADO DE LOS BOTONES POR MEDIO DE SU ID
+const matris1 = (document.getElementById("matris0-0") as HTMLElement);
+const matris2 = (document.getElementById("matris0-1") as HTMLElement);
+const matris3 = (document.getElementById("matris0-2") as HTMLElement);
+const matris4 = (document.getElementById("matris1-0") as HTMLElement);
+const matris5 = (document.getElementById("matris1-1") as HTMLElement);
+const matris6 = (document.getElementById("matris1-2") as HTMLElement);
+const matris7 = (document.getElementById("matris2-0") as HTMLElement);
+const matris8 = (document.getElementById("matris2-1") as HTMLElement);
+const matris9 = (document.getElementById("matris2-2") as HTMLElement);
+
+
