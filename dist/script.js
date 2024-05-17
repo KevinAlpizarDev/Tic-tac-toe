6e5a658e1e7f1df5cd38d8787c4c8b1a8a125e6d
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-05-17 15:26:33 -0600

    Adding ts

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,19 @@
 "use strict";
+{
+}
+console.log(6);
+function backgrounChanger() {
+    document.body.style.backgroundImage = "url('./src/img/background-image-reverse-3000px.jpg')";
+    document.body.style.backgroundSize = "1200px 1200px";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function backgrounChanger2() {
+    document.body.style.backgroundImage = "url('./src/img/first-mode-background.webp')";
+    document.body.style.backgroundSize = "1000px 600px";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function normalBackground() {
+    document.body.style.backgroundImage = "url('./src/img/default-backgound-body.webp')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
